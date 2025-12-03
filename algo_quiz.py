1e64582f8e8acc54215f983adc9f43b63df2fd2b
brendanpeters <bpeters5738@gmail.com>
2025-12-03 10:29:55 -0500

    More questions

--- algo_quiz.py
+++ algo_quiz.py
@@ -145,6 +145,9 @@ PROBLEM_TO_CATEGORY = {
     },
     'Largest Rectangle In Histogram': {
         'category': 'stack',
+        'problem_statement': 'You are given an array of integers heights where heights[i] represents the height of a bar. The width of each bar is 1.\n\nReturn the area of the largest rectangle that can be formed among the bars.\n\nNote: This chart is known as a histogram.',
+        'solution_summary': '',
+        'level': 'hard',
     },
 
     # binary search
@@ -229,6 +232,9 @@ PROBLEM_TO_CATEGORY = {
     },
     'Lowest Common Ancestor of a Binary Search Tree': {
         'category': 'trees',
+        'problem_statement': 'Given a binary search tree (BST) where all node values are unique, and two nodes from the tree p and q, return the lowest common ancestor (LCA) of the two nodes.\n\nThe lowest common ancestor between two nodes p and q is the lowest node in a tree T such that both p and q as descendants. The ancestor is allowed to be a descendant of itself.',
+        'solution_summary': 'DFS(root, p, q). If root, p, or q are null, return null. If p and q are both less than root, search left. If p and q are both greater than root, search right. Otherwise, root must be common ancestor',
+        'level': 'medium',
     },
     'Binary Tree Level Order Traversal': {
         'category': 'trees',
@@ -324,7 +330,7 @@ PROBLEM_TO_CATEGORY = {
     'Palindrome Partitioning': {
         'category': 'backtracking',
         'problem_statement': 'Given a string s, split s into substrings where every substring is a palindrome. Return all possible lists of palindromic substrings.\n\nYou may return the solution in any order.',
-        'solution_summary': '',
+        'solution_summary': 'DFS(i), where i is index in string. Base case - when i >= len(s), append current part to result. Otherwise, loop over j in range(i, len(s)) and at each iteration if isPali, append s[i : j + 1] to part, dfs(j + 1), and s.pop()',
         'level': 'medium',
     },
     'Letter Combinations of a Phone Number': {
@@ -415,15 +421,25 @@ PROBLEM_TO_CATEGORY = {
     # 1D dynamic programming
     'Climbing Stairs': {
         'category': 'dynamic_programming',
+        'problem_statement': 'You are given an integer n representing the number of steps to reach the top of a staircase. You can climb with either 1 or 2 steps at a time.\n\nReturn the number of distinct ways to climb to the top of the staircase.',
+        'solution_summary': 'Base case - If 0 stairs remain, return 1. If less than 0 stairs remain, return 0. Otherwise, dfs(rem - 1) + dfs(rem - 2). Cache with rem as key',
+        'level': 'easy',
     },
     'Min Cost Climbing Stairs': {
         'category': 'dynamic_programming',
+        'problem_statement': 'You are given an array of integers cost where cost[i] is the cost of taking a step from the ith floor of a staircase. After paying the cost, you can step to either the (i + 1)th floor or the (i + 2)th floor.\n\nYou may choose to start at the index 0 or the index 1 floor.\n\nReturn the minimum cost to reach the top of the staircase, i.e. just past the last index in cost.',
+        'solution_summary': 'DFS(stair). Base case - If 0 or fewer stairs remain, return 0 (i.e. if we are past the end of the array). Otherwise, return cost[stair] + min(dfs(stair + 1), dfs(stair + 2)). Cache result for each value of stair. Initialize with min(dfs(0), dfs(1))',
+        'level': 'easy',
     },
     'House Robber': {
         'category': 'dynamic_programming',
+        'problem_statement': 'You are given an integer array nums where nums[i] represents the amount of money the ith house has. The houses are arranged in a straight line, i.e. the ith house is the neighbor of the (i-1)th and (i+1)th house.\n\nYou are planning to rob money from the houses, but you cannot rob two adjacent houses because the security system will automatically alert the police if two adjacent houses were both broken into.\n\nReturn the maximum amount of money you can rob without alerting the police.',
+        'solution_summary': 'DFS(house). Base case - If 0 or fewer houses remain, return 0 (i.e. if we are past the end of the array). Otherwise, return nums[house] + max(dfs(house + 2), dfs(house + 3)). Cache result for each house. Initialize with max(dfs(0), dfs(1))',
+        'level': 'medium',
     },
     'House Robber II': {
         'category': 'dynamic_programming',
+        'problem_statement': 'You are given an integer array nums where nums[i] represents the amount of money the ith house has. The houses are arranged in a circle, i.e. the first house and the last house are neighbors.\n\nYou are planning to rob money from the houses, but you cannot rob two adjacent houses because the security system will automatically alert the police if two adjacent houses were both broken into.\n\nReturn the maximum amount of money you can rob without alerting the police.',
     },
     'Longest Palindromic Substring': {
         'category': 'dynamic_programming',
@@ -433,9 +449,15 @@ PROBLEM_TO_CATEGORY = {
     },
     'Decode Ways': {
         'category': 'dynamic_programming',
+        'problem_statement': 'A string consisting of uppercase english characters can be encoded to a number using the following mapping:\n\n\t'A' -> "1"\n\t'B' -> "2"\n\t...\n\t'Z' -> "26"\n\nTo decode a message, digits must be grouped and then mapped back into letters using the reverse of the mapping above. There may be multiple ways to decode a message. For example, "1012" can be mapped into:\n\n\t"JAB" with the grouping (10 1 2)\n\t"JL" with the grouping (10 12)\n\nThe grouping (1 01 2) is invalid because 01 cannot be mapped into a letter since it contains a leading zero.\n\nGiven a string s containing only digits, return the number of ways to decode it. You can assume that the answer fits in a 32-bit integer.',
+        'solution_summary': 'DFS(i). Base case - if i == len(s) (reached end of string), return 1. If s[i] == \'0\', then string is invalid -> return 0. Otherwise, dfs(i + 1). If not at end of array and s[i] == 1 or s[i] == 2 and s[i + 1] < 7, then dfs(i + 2). Cache on i',
+        'level': 'medium',
     },
     'Coin Change': {
         'category': 'dynamic_programming',
+        'problem_statement': 'You are given an integer array coins representing coins of different denominations (e.g. 1 dollar, 5 dollars, etc) and an integer amount representing a target amount of money.\n\nReturn the fewest number of coins that you need to make up the exact target amount. If it is impossible to make up the amount, return -1.\n\nYou may assume that you have an unlimited number of each coin.',
+        'solution_summary': 'DFS(remaining). Base case - if remaining == 0 return 0. For each coin, if remaining - coin_value > 0, result = min(result, 1 + dfs(remaining - coin_value)',
+        'level': 'medium',
     },
     'Maximum Product Subarray': {
         'category': 'dynamic_programming',
